a2cf19a42075281ef5a21ac58106f856de438748
Oscar Ahlén <github@ahlen.dev>
2024-02-26 20:48:15 +0100

    Add flake default output package

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Bluish color scheme for Neovim";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
     huey = {
       url = "github:oahlen/huey";
       inputs = {
@@ -22,6 +22,13 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
     in {
+      packages.default = pkgs.vimUtils.buildVimPlugin {
+        pname = "iceberg-nvim";
+        version = "1.0.1";
+        src = ./.;
+        meta.homepage = "https://github.com/oahlen/iceberg.nvim/";
+      };
+
       devShells.default = pkgs.mkShell {
         buildInputs = [
           huey.packages.${system}.default
